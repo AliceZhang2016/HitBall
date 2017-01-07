a53be9f4dff6c73af08d64b2e5b9038e3ba74f7c
Nina <ninaann1995@gmail.com>
2017-01-07 12:44:46 +0800

    人物选择

--- app/src/main/java/com/example/nina/test/selectActivity.java
+++ app/src/main/java/com/example/nina/test/selectActivity.java
@@ -7,10 +7,13 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Nina on 17/1/5.
@@ -23,17 +26,76 @@ public class selectActivity extends Activity implements OnClickListener {
     private tab2 people2;
     private LinearLayout mTab1;
     private LinearLayout mTab2;
-
-
+    private ImageView mHero;
+    private LinearLayout next;
+    private LinearLayout pre;
     private FragmentManager fragmentManager;
+    int[] imageId ;
+    private int cur;
+    private Button heroConfirm;
 
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+        final Data app = (Data)getApplication();
         initViews();
         fragmentManager = getFragmentManager();
+        imageId = new int[] { R.drawable.p01, R.drawable.p02,R.drawable.p03,R.drawable.p04,R.drawable.p05,R.drawable.p06 };
+        cur = 0;
+        mHero = (ImageView) findViewById(R.id.hero);
+        heroConfirm = (Button) findViewById(R.id.btn_hero) ;
+        next = (LinearLayout) findViewById(R.id.rightChange);
+        pre = (LinearLayout) findViewById(R.id.leftChange);
+
+        //默认为一
+        resetBtn();
+        ((ImageButton) mTab1.findViewById(R.id.btn_tab_bottom_tab1))
+                .setImageResource(R.drawable.tab_find_frd_pressed);
+        FragmentTransaction transactioni = fragmentManager.beginTransaction();
+        hideFragments(transactioni);
+
+        if (people1 == null){
+        people1 = new tab();
+        transactioni.add(R.id.id_content, people1);}
+        else transactioni.show(people1);
+        mHero.setImageResource(imageId[app.getHero1()]);
+        transactioni.commit();
+
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (cur<5){
+                cur++;
+                mHero.setImageResource(imageId[cur]);}
+                else{
+                    mHero.setImageResource(R.drawable.p06);
+                }
+            }
+        });
+        pre.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (cur>0){
+                    cur--;
+                    mHero.setImageResource(imageId[cur]);}
+                else{
+                    mHero.setImageResource(R.drawable.p01);
+                }
+            }
+        });
+        heroConfirm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(people1.isVisible()){
+                    app.setHero1(cur);
+                }
+                else if(people2.isVisible()){
+                    app.setHero2(cur);
+                }
+            }
+        });
     }
     private void initViews()
     {
@@ -69,12 +131,13 @@ public class selectActivity extends Activity implements OnClickListener {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         // 先隐藏掉所有的Fragment，以防止有多个Fragment显示在界面上的情况
         hideFragments(transaction);
+        final Data app = (Data)getApplication();
         switch (index)
         {
             case 0:
                 ((ImageButton) mTab1.findViewById(R.id.btn_tab_bottom_tab1))
                         .setImageResource(R.drawable.tab_find_frd_pressed);
-
+                mHero.setImageResource(imageId[app.getHero1()]);
                 if (people1 == null)
                 {
                     // 如果MessageFragment为空，则创建一个并添加到界面上
@@ -90,7 +153,7 @@ public class selectActivity extends Activity implements OnClickListener {
             case 1:
                 ((ImageButton) mTab2.findViewById(R.id.btn_tab_bottom_tab2))
                         .setImageResource(R.drawable.tab_find_frd_pressed);
-
+                mHero.setImageResource(imageId[app.getHero2()]);
                 if (people2 == null)
                 {
                     // 如果MessageFragment为空，则创建一个并添加到界面上
